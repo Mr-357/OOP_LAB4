f1580d35608c4db9f86b2b758d9f1aee002caa40
Mihajlo <Mihajlo@DESKTOP-QV9P8RR>
2018-12-04 20:50:26 +0100

    Add project files.

---
+++ Cards/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cards
+{
+    public enum Color
+    {
+        Spade,
+        Heart,
+        Club,
+        Diamond
+    }
+    class Card : PictureBox
+    {
+        private int value;
+        private Color color;
+
+    }
+}
